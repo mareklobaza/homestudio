1eba84487087181b217e8ccc3310f69a7632a092
Marek Lobaza <marek.lobaza@gmail.com>
2024-01-07 13:35:52 +0000

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # homestudio
 pip install -r requirements.txt
+python3 slideshow.py ~/mnt/gdrive/lobaza63/homestudio/
+@reboot python3 /home/marek/sources/homestudio/slideshow.py ~/mnt/gdrive/lobaza63/homestudio/ &
--- slideshow.py
+++ slideshow.py
@@ -22,7 +22,7 @@ root=tk.Tk()
 root.geometry("1920x1080")
 
 for file in dir_list:
-    images.append(ImageTk.PhotoImage(Image.open(path + '/' + file))) #this maybe not the perfect idea, may cause a lot of memory problems
+   images.append(ImageTk.PhotoImage(Image.open(path + '/' + file))) #this maybe not the perfect idea, may cause a lot of memory problems
 
 l=Label()
 l.pack()
@@ -30,18 +30,22 @@ l.pack()
 root.attributes('-fullscreen', True)
 
 # using recursion to slide to next image 
+
+print("Starting the loop")
+
 x = 1
 def move():
     global x
     if x == len(images):
         x = 1
+
     l.config(image=images[x])
     x = x+1
 
-    root.after(5000, move)
+    root.after(50000, move)
 
 # calling the function
-move()
+move()  
 
 root.mainloop()
 # function to change to next image 
